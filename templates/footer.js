335f32b10a7597a462e16776c747a2290b4fdd19
Jens Albert <ja@8works.de>
2015-08-27 18:10:02 +0200

    Custom template files

---
+++ templates/footer.js
@@ -0,0 +1,7 @@
+/**
+ * Your markdown-themeable-pdf custom footer
+ */
+module.exports = {
+    height: "2cm",
+    contents: '<div style="text-align: center;">Page {{page}}</span>/<span>{{pages}}</div>'
+};
---
+++ templates/header.js
@@ -0,0 +1,7 @@
+/**
+ * Your markdown-themeable-pdf custom header
+ */
+module.exports = {
+    height: "100px",
+    contents: '<div style="text-align: right;">markdown-themeable-pdf</div>'
+};
