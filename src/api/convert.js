911778c9eda576e08e22adf3a6106f07e7264ca6
Jens A <cakebake.dev@gmail.com>
2018-02-28 21:14:34 +0100

    Easier switch between export format

--- src/api/convert.js
+++ src/api/convert.js
@@ -9,48 +9,15 @@ const convert = (filePath, exportFileType, fileInfo, encoding) => {
     if (!isEmpty(filePath)) {
       encoding = await detectFileEncoding(filePath, encoding)
       try {
+        const htmlIsFinalFormat = (exportFileType === 'html')
         const markdown = await readFileContent(filePath, encoding)
-        switch (exportFileType) {
-          case 'html':
-            try {
-              const exportFile = await markdownToHTML(markdown, true)
-              resolve(exportFile)
-            } catch (e) {
-              reject(e)
-            }
-            break
-          case 'pdf':
-            try {
-              const html = await markdownToHTML(markdown)
-              console.log(html)
-              resolve(filePath)
-            } catch (e) {
-              reject(e)
-            }
-            break
-          case 'jpeg':
-            try {
-              const html = await markdownToHTML(markdown)
-              console.log(html)
-              resolve(filePath)
-            } catch (e) {
-              reject(e)
-            }
-            break
-          case 'png':
-            try {
-              const html = await markdownToHTML(markdown)
-              console.log(html)
-              resolve(filePath)
-            } catch (e) {
-              reject(e)
-            }
-            break
-          default:
-            reject(`Export file type "${exportFileType}" is not supported`)
+        const html = await markdownToHTML(markdown, htmlIsFinalFormat)
+        if (htmlIsFinalFormat) {
+          resolve(html)
+        } else {
+          resolve(html)
         }
       } catch (e) {
-        console.error(e);
         reject(e)
       }
     } else {
