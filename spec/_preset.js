a7fe062f31f269b43d0a937f41832cd6c97f04f3
8works (Jens Albert) <ja@8works.de>
2018-03-07 10:46:40 +0100

    Refactoring options

--- spec/_preset.js
+++ spec/_preset.js
@@ -1,51 +1,35 @@
 'use babel'
 
-import { merge } from 'lodash'
+import { get, merge } from 'lodash'
 import { join, resolve as pathResolve } from 'path'
 import { readFile, getFileDirectory } from '../lib/api/filesystem'
-import { getConfig } from '../lib/api/atom'
+import { options as _options } from '../lib/api/atom'
 import markdownToHTML from '../lib/api/convert/markdownToHTML'
 
-let currentMdFilePath = ''
+let _currentMdFilePath = ''
 
-export const getCurrentMdFilePath = () => currentMdFilePath
+export const options = _options(true)
 
 // fake for spec suit, because atom project path points to /tmp -.-
 export const getProjectRootPath = () => pathResolve(__dirname, '..')
 
-export const getCustomStylesPath = () => getConfig('customStylesPath', true)
+export const getCustomStylesPath = () => get(options, 'customStylesPath')
 
-export const getcodeHighlightingTheme = () => getConfig('codeHighlightingTheme', true)
+export const getcodeHighlightingTheme = () => get(options, 'codeHighlightingTheme')
 
-export const markdownItOptions = () => {
-  return {
-    html: getConfig('enableHtmlInMarkdown', true),
-    linkify: getConfig('enableLinkify', true),
-    typographer: getConfig('enableTypographer', true),
-    xhtmlOut: getConfig('enableXHTML', true),
-    breaks: getConfig('enableBreaks', true),
-    quotes: getConfig('smartQuotes', true),
-    enableCodeHighlighting: getConfig('enableCodeHighlighting', true),
-    codeHighlightingAuto: getConfig('codeHighlightingAuto', true),
-    enableImSizeMarkup: getConfig('enableImSizeMarkup', true),
-    enableCheckboxes: getConfig('enableCheckboxes', true),
-    enableSmartArrows: getConfig('enableSmartArrows', true),
-    enableTOC: getConfig('enableTocAndAnchor', true) === 'TOC enabled' || getConfig('enableTocAndAnchor', true) === 'TOC and Anchors enabled',
-    enableAnchor: getConfig('enableTocAndAnchor', true) === 'Anchors enabled' || getConfig('enableTocAndAnchor', true) === 'TOC and Anchors enabled',
-    tocFirstLevel: getConfig('tocFirstLevel', true),
-    tocLastLevel: getConfig('tocLastLevel', true),
-    enableEmoji: getConfig('enableEmoji', true),
-    enableFootnotes: getConfig('enableFootnotes', true)
-  }
-}
+export const markdownItOptions = () => get(options, 'markdownIt')
 
-export const getMarkdownTestFilePath = (testFile) => join(__dirname, 'markdown', testFile)
+export const setCurrentMdFilePath = (path) => _currentMdFilePath = path
+
+export const getCurrentMdFilePath = () => _currentMdFilePath
 
 export const getMarkdown = (testFile) => {
-  currentMdFilePath = getMarkdownTestFilePath(testFile)
-  return readFile(currentMdFilePath)
+  setCurrentMdFilePath(getMarkdownTestFilePath(testFile))
+  return readFile(getCurrentMdFilePath())
 }
 
 export const getHtml = (markdown, _options = {}, isFinalFormat = true) => {
-  return markdownToHTML(markdown, isFinalFormat, merge(markdownItOptions(), _options), getFileDirectory(currentMdFilePath))
+  return markdownToHTML(markdown, isFinalFormat, merge(markdownItOptions(), _options), getFileDirectory(getCurrentMdFilePath()))
 }
+
+export const getMarkdownTestFilePath = (testFile) => join(__dirname, 'markdown', testFile)
