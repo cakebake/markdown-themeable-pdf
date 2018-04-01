3c6b94bb86a7c5420524fe32459dd49ebc7705ed
cakebake <cakebake.dev@gmail.com>
2018-04-01 17:27:52 +0200

    Obsolete table innerWrap

--- spec/010-markdown-to-html-table-spec.js
+++ spec/010-markdown-to-html-table-spec.js
@@ -32,42 +32,4 @@ describe('Table', () => {
       expect(html).toMatch(escapeRegExp('<th>cdID</th>\n<th>artID</th>'))
     })
   })
-
-  it('checks disabled table cell innerWrap', () => {
-    let html = ''
-    runs(async () => {
-      try {
-        const md = await getMarkdown('table.md')
-        html = await getHtml(md, {}, true)
-      } catch (e) {
-        throw e
-      }
-    })
-    waitsFor(() => {
-      return html
-    }, 'Should get html')
-    runs(() => {
-      expect(html).toMatch(escapeRegExp('<th>hello</th>'))
-      expect(html).toMatch(escapeRegExp('<td>1</td>'))
-    })
-  })
-
-  it('checks enabled table cell innerWrap', () => {
-    let html = ''
-    runs(async () => {
-      try {
-        const md = await getMarkdown('table.md')
-        html = await getHtml(md, {}, false)
-      } catch (e) {
-        throw e
-      }
-    })
-    waitsFor(() => {
-      return html
-    }, 'Should get html')
-    runs(() => {
-      expect(html).toMatch(escapeRegExp('<th><div>hello</div></th>'))
-      expect(html).toMatch(escapeRegExp('<td><div>1</div></td>'))
-    })
-  })
 })
