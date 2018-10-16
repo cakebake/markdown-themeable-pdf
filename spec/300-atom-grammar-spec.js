cc8d125d3cbc4d88e30815c7827854c6fefcb105
cakebake <cakebake.dev@gmail.com>
2018-10-16 14:42:32 +0200

    Test default gfm grammar

--- spec/300-atom-grammar-spec.js
+++ spec/300-atom-grammar-spec.js
@@ -7,7 +7,7 @@
 //
 // Tests are written with https://jasmine.github.io/1.3/introduction.html
 
-describe('Grammar', () => {
+fdescribe('Grammar', () => {
   it('parses the grammar', () => {
     let grammar = null
     runs(async () => {
@@ -26,6 +26,7 @@ describe('Grammar', () => {
       expect(grammar.scopeName).toBe('source.gfm')
     })
   })
+
   it('tokenizes YAML front matter', () => {
     let grammar = null
     runs(async () => {
@@ -54,6 +55,53 @@ describe('Grammar', () => {
         value: '--- [markdown-themeable-pdf] options; -->',
         scopes: ['source.gfm', 'markdown-themeable-pdf.front-matter.yaml.gfm', 'comment.hr.gfm']
       })
+    })
+  })
+
+  it('checks extended source.gfm is equal standard source.gfm', () => {
+    const grammar = {}
+    let fin = false
+    runs(async () => {
+      try {
+        await atom.packages.activatePackage('language-gfm')
+        grammar.default = await atom.grammars.grammarForScopeName('source.gfm')
+        await atom.packages.activatePackage('markdown-themeable-pdf')
+        grammar.extended = await atom.grammars.grammarForScopeName('source.gfm')
+        fin = true
+      } catch (e) {
+        throw e
+      }
+    })
+    waitsFor(() => {
+      return fin
+    }, 'Should set grammar')
+    runs(() => {
+      const content = '# headline\n<!-- some -->\n`code`'
+      const [
+        firstLineTokensDefault,
+        secondLineTokensDefault,
+        thirdLineTokensDefault
+      ] = grammar.default.tokenizeLines(content)
+      const [
+        firstLineTokensExtended,
+        secondLineTokensExtended,
+        thirdLineTokensExtended
+      ] = grammar.extended.tokenizeLines(content)
+      expect(firstLineTokensExtended[0]).toEqual(firstLineTokensDefault[0])
+      expect(secondLineTokensExtended[0]).toEqual(secondLineTokensDefault[0])
+      expect(thirdLineTokensExtended[0]).toEqual(thirdLineTokensDefault[0])
+      // expect(firstLineTokensExtended[0]).toEqual({
+      //   value: '#',
+      //   scopes: ['source.gfm', 'markup.heading.heading-1.gfm', 'markup.heading.marker.gfm']
+      // })
+      // expect(secondLineTokensDefault[0]).toEqual({
+      //   value: '<!--',
+      //   scopes: ['source.gfm', 'comment.block.gfm', 'punctuation.definition.comment.gfm']
+      // })
+      // expect(thirdLineTokensDefault[0]).toEqual({
+      //   value: '`',
+      //   scopes: ['source.gfm', 'markup.raw.gfm']
+      // })
     })
   })
 })
