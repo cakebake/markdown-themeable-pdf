a3ffd31b284d2872bfeeaad7c96af9bc899dd753
Jens A <cakebake.dev@gmail.com>
2019-02-03 21:38:44 +0100

    Adds front matter custom configuration to each file

--- spec/110-custom-options-spec.js
+++ spec/110-custom-options-spec.js
@@ -1,7 +1,14 @@
 'use babel'
 
-import { addOptionsToContent, getMatterDelimiters } from '../lib/api/atom/customOptions'
+import {
+  removeOptionsFromContent,
+  addOptionsToContent,
+  getMatterDelimiters,
+  getOptionsFromContent,
+  mergeOptionsWithFileOptions
+} from '../lib/api/atom/customOptions'
 import { escapeRegExp } from 'lodash'
+import { getMarkdownTestFilePath } from './_preset'
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
@@ -36,4 +43,95 @@ describe('Custom options', () => {
       expect(contentWithOptions).toMatch(escapeRegExp('ipsum: 2'))
     })
   })
+  it('should remove front matter from content', () => {
+    let contentNoOptions = ''
+    const [startDelimiter, endDelimiter] = getMatterDelimiters()
+    const content = [
+      startDelimiter,
+      'lorem: 1',
+      'ipsum: 2',
+      endDelimiter,
+      '# markdown',
+      'with some content'
+    ].join('\n')
+    runs(async () => {
+      try {
+        contentNoOptions = await removeOptionsFromContent(content)
+      } catch (e) {
+        throw e
+      }
+    })
+    waitsFor(() => {
+      return contentNoOptions
+    }, 'Should remove front matter')
+    runs(() => {
+      expect(contentNoOptions).not.toMatch(escapeRegExp(startDelimiter))
+      expect(contentNoOptions).not.toMatch(escapeRegExp(endDelimiter))
+      expect(contentNoOptions).not.toMatch(escapeRegExp('lorem: 1'))
+      expect(contentNoOptions).not.toMatch(escapeRegExp('ipsum: 2'))
+    })
+  })
+  it('should read front matter from content', () => {
+    let options = null
+    const [startDelimiter, endDelimiter] = getMatterDelimiters()
+    const content = [
+      startDelimiter,
+      'lorem: 1',
+      'ipsum: 2',
+      endDelimiter,
+      '# markdown',
+      'with some content'
+    ].join('\n')
+    runs(async () => {
+      try {
+        options = await getOptionsFromContent(content)
+      } catch (e) {
+        throw e
+      }
+    })
+    waitsFor(() => {
+      return options
+    }, 'Should get front matter')
+    runs(() => {
+      expect(options).toEqual({ lorem: 1, ipsum: 2 })
+    })
+  })
+  it('should merge front matter from file', () => {
+    let options = null
+    const preOpts = { lorem: 1, ipsum: 2, dolor: 3 }
+    const filePath = getMarkdownTestFilePath('front-matter.md')
+    runs(async () => {
+      try {
+        options = await mergeOptionsWithFileOptions(filePath, preOpts)
+      } catch (e) {
+        throw e
+      }
+    })
+    waitsFor(() => {
+      return options
+    }, 'Should merge options with front matter')
+    runs(() => {
+      expect(options).toEqual({ lorem: 1, ipsum: 10, dolor: 3 })
+    })
+  })
+  // it(`should overwrite options with custom options on convert`, () => {
+  //   let convertedFilePath
+  //   const markdownFilePath = getMarkdownTestFilePath('front-matter.md')
+  //   runs(async () => {
+  //     const type = 'pdf'
+  //     const options = getOptions()
+  //     const destinationPath = getDefaultExportFilePath(markdownFilePath, type)
+  //     removeSync(destinationPath)
+  //     expect(existsSync(destinationPath)).toBe(false)
+  //     convertedFilePath = await convert(markdownFilePath, destinationPath, type, options)
+  //   })
+  //   waitsFor(() => {
+  //     return convertedFilePath
+  //   }, 'Should convert markdown', timeout)
+  //   runs(() => {
+  //     expect(existsSync(convertedFilePath)).toBe(true)
+  //     expect(extname(convertedFilePath)).toBe('.pdf')
+  //     expect(getSizeOfPdf(convertedFilePath).pages).toBeGreaterThan(2)
+  //   })
+  // })
 })
---
+++ spec/markdown/custom-options.md
@@ -0,0 +1,15 @@
+---
+theme:
+  enableDefaultStyles: false
+  customStylesPath: spec/markdown/files/styles.css
+  customHeaderPath: spec/markdown/files/pdfHeader.html
+  customFooterPath: spec/markdown/files/pdfFooter.html
+jpeg:
+  quality: 90
+  width: 500px
+  height: 500px
+imageExportFileType: jpeg
+---
+# Custom options with front matter
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
